fbfe3b95d1a664337b38a3f5002acf8832a966bf
dvym07-qwerty <divyam1080.be25@chitkarauniversity.edu.in>
2026-05-03 01:08:37 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,27 @@
-function login(){
-    let email=document.getElementById("email").value.trim()
-    let password=document.getElementById("pass").value.trim()
-    // let  error = document.getElementById('error');
+function login() {
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('pass').value.trim();
+    const error = document.getElementById('error');
     const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (email=== '' || pass === ''){
-        document.getElementById('error').style.display = 'block';
+    if (email === '' || password === '') {
+        error.textContent = '⚠️ Please fill in both fields!';
+        error.style.display = 'block';
         return;
-}
-    if (!emailFormat.test(email)) {
-    error.textContent = '⚠️ Please enter a valid email like: you@example.com';
-    error.style.display = 'block';
-    return;
-    
-}
+    }
 
-window.location.href = 'home.html'; 
+    if (!emailFormat.test(email)) {
+        error.textContent = '⚠️ Please enter a valid email like: you@example.com';
+        error.style.display = 'block';
+        return;
+    }
+
+    if (password.length < 7) {
+        error.textContent = '⚠️ Password must be at least 7 characters!';
+        error.style.display = 'block';
+        return;
+    }
+
+    error.style.display = 'none';
+    window.location.href = 'home.html';
 }
