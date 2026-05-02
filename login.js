9757d559194eee4213726374cdf4f841b91f9452
dvym07-qwerty <divyam1080.be25@chitkarauniversity.edu.in>
2026-05-03 00:15:02 +0530

    made a login page

---
+++ login.js
@@ -0,0 +1,18 @@
+function login(){
+    let email=document.getElementById("email").value.trim()
+    let password=document.getElementById("pass").value.trim()
+    // let  error = document.getElementById('error');
+    const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email=== '' || pass === ''){
+        document.getElementById('error').style.display = 'block';
+        return;
+}
+    if (!emailFormat.test(email)) {
+    error.textContent = '⚠️ Please enter a valid email like: you@example.com';
+    error.style.display = 'block';
+    return;
+     
+}
+window.location.href = 'index.html'; 
+}
