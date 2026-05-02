2f6ac6fa213954af419b990f7ec5a1e6845c9b8d
dvym07-qwerty <divyam1080.be25@chitkarauniversity.edu.in>
2026-05-03 01:34:39 +0530

    added signup option

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
+function showSignup() {
+    document.getElementById('login-box').style.display = 'none';
+    document.getElementById('signup-box').style.display = 'flex';
+}
+function showLogin() {
+    document.getElementById('signup-box').style.display = 'none';
+    document.getElementById('login-box').style.display = 'flex';
+}
+
+
 function login() {
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('pass').value.trim();
@@ -21,7 +31,48 @@ function login() {
         error.style.display = 'block';
         return;
     }
+const savedEmail = localStorage.getItem('userEmail');
+const savedPassword = localStorage.getItem('userPassword');
+
+    if (email !== savedEmail || password !== savedPassword) {
+        error.textContent = '⚠️ Email or Password is wrong!';
+        error.style.display = 'block';
+        return;
+    }
 window.location.href = 'home.html';
 
     
+}
+function signup() {
+    const name = document.getElementById('signup-name').value.trim();
+    const email = document.getElementById('signup-email').value.trim();
+    const password = document.getElementById('signup-pass').value.trim();
+    const error = document.getElementById('signup-error');
+    const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (name === '' || email === '' || password === '') {
+        error.textContent = '⚠️ Please fill in all fields!';
+        error.style.display = 'block';
+        return;
+    }
+
+    if (!emailFormat.test(email)) {
+        error.textContent = '⚠️ Please enter a valid email like: you@example.com';
+        error.style.display = 'block';
+        return;
+    }
+
+    if (password.length < 7) {
+        error.textContent = '⚠️ Password must be at least 7 characters!';
+        error.style.display = 'block';
+        return;
+    }
+
+    // Save to localStorage
+    localStorage.setItem('userEmail', email);
+    localStorage.setItem('userPassword', password);
+    localStorage.setItem('userName', name);
+
+    alert('✅ Account created! Please log in.');
+    showLogin();
 }
